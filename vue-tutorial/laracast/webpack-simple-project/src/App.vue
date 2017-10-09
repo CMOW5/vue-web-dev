31d2984b3586b44195069ede0d8ff8236cda5832
CMOW5 <tec.cmos@gmail.com>
2017-10-09 14:38:53 -0500

    webpack simple project and vue cli

---
+++ vue-tutorial/laracast/webpack-simple-project/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+
+  <div id="app">
+
+    <message>Hello there</message>
+    <message>Hello again</message>
+   
+  </div>
+
+</template>
+
+<script>
+
+import Message from './components/Message.vue';
+
+export default {
+
+  components : {
+
+    Message
+
+  },
+
+  name: 'app',
+  data () {
+    return {
+      
+    }
+  }
+
+}
+
+</script>
+
+<style lang="scss">
+
+
+</style>
---
+++ vue-tutorial/laracast/webpack-simple-project/src/components/Message.vue
@@ -0,0 +1,31 @@
+<template>
+	
+	<div class="box">
+		
+		<p>
+
+			<slot></slot>
+		
+		</p>
+
+	</div>
+
+</template>
+
+
+<script>
+	
+	export default {
+
+	}
+
+
+</script>
+
+<style>
+	
+	.box{
+		background: green; padding: 10px; border: 1px solid red; margin-bottom: 5px	;
+	}
+
+</style>
